cd5bf73c7663224dfef50712eb88f90a498ad091
Driesboy <coenendries3@gmail.com>
2016-12-12 17:14:52 +0100

    Update SAC.php

--- src/DarkWav/SAC/SAC.php
+++ src/DarkWav/SAC/SAC.php
@@ -116,6 +116,104 @@ class SAC extends PluginBase
         }
       }
     }
+	if(strtolower($cmd->getName()) === "report"){
+        	if(!(isset($args[0]))){
+          		$sender->sendMessage(TF::RED . "Error: Not Enough Parameters. Usage: /report <player>");
+          		return true;
+        	}else{
+         	 $sender_name = $sender->getName();
+         	 $sender_display_name = $sender->getDisplayName();
+         	 $name = $args[0];
+         	 $player = $this->getServer()->getPlayer($name);
+         	 if($player === null){
+         	   foreach($this->getServer()->getOnlinePlayers() as $p){
+          	    if($p->hasPermission("gmcpe.staff")){
+              		  $p->sendMessage(TF::YELLOW . $sender_name . " reported " . $name . " for using hacks and/or mods!");
+             		}
+            	}
+            	$sender->sendMessage(TF::GREEN . "The Report has Been Sent to All Online Staffs.");
+            	return true;
+          }else{
+            foreach($this->getServer()->getOnlinePlayers() as $p){
+              	if($p->hasPermission("gmcpe.staff")){
+                	$p->sendMessage(TF::YELLOW . $sender_name . " reported " . $name . " for using hacks and/or mods!");
+             	 }
+            }
+            $player->sendMessage(TF::YELLOW . $sender_name . " has reported you for using hacks and/or mods!");
+            $sender->sendMessage(TF::GREEN . "The Report has Been Sent to All Online Staffs.");
+            return true;
+          }
+        }
+      }
+    }
+    if(strtolower($cmd->getName()) === "warn") {
+        if(!(isset($args[0]) and isset($args[1]))) {
+          $sender->sendMessage(TF::RED . "Error: not enough args. Usage: /warn <player> <reason>");
+          return true;
+        } else {
+          $sender_name = $sender->getName();
+          $name = $args[0];
+          $player = $this->getServer()->getPlayer($name);
+          if($player->isOp()){
+            $sender->sendMessage(TF::RED . $player->getName() . " has the OP status and thus, cannot be warned.");
+            return;
+          }
+          if($player === null) {
+            $sender->sendMessage(TF::RED . "Player " . $name . " could not be found.");
+            return true;
+          } else {
+            unset($args[0]);
+            $player_name = $player->getName();
+            if(!(file_exists($this->dataPath() . "Players/" . strtolower($player_name) . ".txt"))) {
+              touch($this->dataPath() . "Players/" . strtolower($player_name) . ".txt");
+              file_put_contents($this->dataPath() . "Players/" . strtolower($player_name) . ".txt", "0");
+            }
+            $reason = implode(" ", $args);
+            $file = file_get_contents($this->dataPath() . "Players/" . strtolower($player_name) . ".txt");
+            if($file >= "3") {
+              $string = "action_after_three_warns: ";
+              $action = substr(strstr(file_get_contents($this->dataPath() . "config.yml"), $string), strlen($string));
+                if($player->isOP()){
+                  return;
+                }
+                $player->setBanned(true);
+                $sender->sendMessage(TF::GREEN . $player_name . " was banned for being warned 3+ times.");
+                return true;
+            } else {
+              $player->sendMessage(TF::YELLOW . "You have been warned by " . $sender_name . " for " . $reason);
+              $this->getServer()->broadcastMessage(TF::YELLOW . $player_name . " was warned by " . $sender_name . " for " . $reason);
+              $file = file_get_contents($this->dataPath() . "Players/" . strtolower($player_name) . ".txt");
+              file_put_contents($this->dataPath() . "Players/" . strtolower($player_name) . ".txt", $file + 1);
+              $sender->sendMessage(TF::GREEN . "Warned " . $player_name . ", and added +1 warns to their file.");
+              return true;
+            }
+          }
+        }
+      }
+      if(strtolower($cmd->getName()) === "warns") {
+        if(!(isset($args[0]))) {
+          $sender->sendMessage(TF::RED . "Error: not enough args. Usage: /warns <player>");
+          return true;
+        } else {
+          $name = $args[0];
+          $player = $this->getServer()->getPlayer($name);
+          if($player === null) {
+            $sender->sendMessage(TF::RED . "Player " . $name . " could not be found.");
+            return true;
+          } else {
+            $player_name = $player->getName();
+            if(!(file_exists($this->dataPath() . "Players/" . strtolower($player_name) . ".txt"))) {
+              $sender->sendMessage(TF::RED . $player_name . " has no warns.");
+              return true;
+            } else {
+              $player_warns = file_get_contents($this->dataPath() . "Players/" . strtolower($player_name) . ".txt");
+              $sender->sendMessage(TF::GREEN . "Player " . $player_name . " has " . $player_warns . " warns.");
+              return true;
+            }
+          }
+        }
+      }
+    }
   }
   
   public function NotifyAdmins($message)
